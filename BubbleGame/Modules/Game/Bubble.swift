bffff3598a0d9484867ea127377c4275eacd5012
Dhruv Saraswat <saraswat.dhruv@gmail.com>
2024-05-14 11:13:01 +0530

    This commit contains the following changes :- 1. Added the implementation to create the bubble wherever the user taps on the BubblesViewController

---
+++ BubbleGame/Modules/Game/Bubble.swift
@@ -0,0 +1,60 @@
+//
+//  Bubble.swift
+//  BubbleGame
+//
+//  Created by Dhruv Saraswat on 14/05/24.
+//
+
+import UIKit
+
+final class Bubble: UIView {
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "10"
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    override class var requiresConstraintBasedLayout: Bool {
+        true
+    }
+
+    private func setupView() {
+        backgroundColor = .white.withAlphaComponent(0.7)
+        addSubview(label)
+        addConstraints()
+        setupActions()
+        makeCircular()
+    }
+
+    private func addConstraints() {
+        label.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+
+    private func setupActions() {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(resetCounter))
+        addGestureRecognizer(gestureRecognizer)
+    }
+
+    private func makeCircular() {
+        clipsToBounds = true
+        layer.cornerRadius = bounds.height / 2
+    }
+
+    @objc private func resetCounter() {
+
+    }
+}
--- BubbleGame/Modules/Game/BubblesViewController.swift
+++ BubbleGame/Modules/Game/BubblesViewController.swift
@@ -14,10 +14,17 @@ final class BubblesViewController: UIViewController {
     private let queue = DispatchQueue(label: "com.timer")
     private let utility = Utility()
 
+    private lazy var bubbleWidth: CGFloat = {
+        (self.view.window?.windowScene?.screen.bounds.size.width ?? UIScreen.main.bounds.width) / 4
+    }()
+
+    private var tapGesture: UITapGestureRecognizer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         hideIntroductoryCountdownLabel()
         hideCountdownLabel()
+        removeTapGestureRecognizer()
         // Do any additional setup after loading the view.
     }
 
@@ -42,6 +49,7 @@ final class BubblesViewController: UIViewController {
         }, countdownCompletion: { [unowned self] in
             self.hideIntroductoryCountdownLabel()
             self.showCountdownLabel()
+            self.addTapGestureRecognizer()
         })
     }
 
@@ -67,5 +75,54 @@ final class BubblesViewController: UIViewController {
         DispatchQueue.main.async { [unowned self] in
             self.countdownLabel.isHidden = true
         }
+    }
+
+    private func addTapGestureRecognizer() {
+        DispatchQueue.main.async { [unowned self] in
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(createBubble))
+            tapGesture = gesture
+            view.addGestureRecognizer(gesture)
+        }
+    }
+
+    private func removeTapGestureRecognizer() {
+        DispatchQueue.main.async { [unowned self] in
+            if let tapGesture {
+                view.removeGestureRecognizer(tapGesture)
+            }
+        }
+    }
+
+    @objc private func createBubble(_ touch: UITapGestureRecognizer) {
+        let touchPoint = touch.location(in: view)
+        var xCenter = touchPoint.x - (bubbleWidth / 2)
+        var yCenter = touchPoint.y - (bubbleWidth / 2)
+
+        /**if xCenter < (bubbleWidth / 2) {
+            /// some part of the left side of the bubble will be outside the screen's edge,
+            /// so shift the bubble center a little to the right so that the entire bubble is visible
+            xCenter = (bubbleWidth / 2) + 5
+        }
+
+        if xCenter > (UIScreen.main.bounds.width - (bubbleWidth / 2)) {
+            /// some part of the right side of the bubble will be outside the screen's edge,
+            /// so shift the bubble center a little to the left so that the entire bubble is visible
+            xCenter = (UIScreen.main.bounds.width - (bubbleWidth / 2)) - 5
+        }
+
+        if yCenter < (bubbleWidth / 2) {
+            /// some part of the top side of the bubble will be outside the screen's edge,
+            /// so shift the bubble center a little down so that the entire bubble is visible
+            yCenter = (bubbleWidth / 2) + 5
+        }
+
+        if yCenter > (UIScreen.main.bounds.height - (bubbleWidth / 2)) {
+            /// some part of the down side of the bubble will be outside the screen's edge,
+            /// so shift the bubble center a little up so that the entire bubble is visible
+            yCenter = (UIScreen.main.bounds.height - (bubbleWidth / 2)) - 5
+        }*/
+
+        let bubble = Bubble(frame: CGRect(x: xCenter, y: yCenter, width: bubbleWidth, height: bubbleWidth))
+        view.addSubview(bubble)
     }
 }
